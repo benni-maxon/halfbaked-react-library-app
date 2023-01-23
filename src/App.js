3963edabdf08abc63676ffa69d72024b570c9c68
benni maxon <benni.maxon@gmail.com>
2023-01-23 15:47:59 -0800

    added Switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail.js';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/" component={BookList} />
+        <Route exact path="/:id" component={BookDetail} />
+      </Switch>
     </main>
   );
 }
